b9ec0cac03d0ad6af248f576543358ecfe7e72b9
Ori71e2 <Ori71e2@gmail.com>
2019-12-26 23:21:03 +0800

    添加tree控件，修复滚动条bug

--- src/main/components/fileWatch/fileWatch.js
+++ src/main/components/fileWatch/fileWatch.js
@@ -12,24 +12,24 @@ function addIpcListener() {
 }
 function fileBrowserListener(e, { action, data }) {
   let appDir = os.homedir()
-    try {
-      if (action === 'getFiles') {
-        fs.watch(appDir, { persistent: true, recursive: true }, (eventType, filename) => {
-          console.log(filename)
-          console.log(global.mainGlobalMianWindow)
-          /**
-           * 此处必须捕获异常，否则当页面被销毁，此处未销毁，会报错。
-           * 可以在页面销毁时，同步销毁此处。
-           */
-          try {
-            e.reply('file-browser-reply', new Message(filename, filename))
-          } catch (err) {
-            console.log('renderer page has been destroyed!')
-            ipcMain.removeLinstener('file-browser', fileBrowserListener)
-          }
-        })
-      }
-    } catch (err) {
+  try {
+    if (action === 'getFiles') {
+      fs.watch(appDir, { persistent: true, recursive: true }, (eventType, filename) => {
+        console.log(filename)
+        console.log(global.mainGlobalMianWindow)
+        /**
+         * 此处必须捕获异常，否则当页面被销毁，此处未销毁，会报错。
+         * 可以在页面销毁时，同步销毁此处。
+         */
+        try {
+          e.reply('file-browser-reply', new Message(filename, filename))
+        } catch (err) {
+          console.log('renderer page has been destroyed!')
+          ipcMain.removeLinstener('file-browser', fileBrowserListener)
+        }
+      })
     }
+  } catch (err) {
+  }
 }
 addIpcListener()
--- src/renderer/layout/fileDisplay/index.vue
+++ src/renderer/layout/fileDisplay/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="path">
-    <!-- <h3>File Browser</h3> -->
-    <span>{{ pathInfo }}</span>
+  <div class="custom-tree-container">
+    <div>
+      <el-tree
+        :data="data"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+        :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ node.label }}</span>
+          <span>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => append(data)">
+              Append
+            </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => remove(node, data)">
+              Delete
+            </el-button>
+          </span>
+        </span>
+      </el-tree>
+    </div>
   </div>
 </template>
 
@@ -9,11 +33,48 @@
 import { ipcRenderer } from 'electron'
 import Message from '@/../utils/message.js'
 const path = require('path')
+let id = 1000
 export default {
   name: 'file-browser',
   data() {
+    const data = [{
+      id: 1,
+      label: '一级 1',
+      children: [{
+        id: 4,
+        label: '二级 1-1',
+        children: [{
+          id: 9,
+          label: '三级 1-1-1'
+        }, {
+          id: 10,
+          label: '三级 1-1-2'
+        }]
+      }]
+    }, {
+      id: 2,
+      label: '一级 2',
+      children: [{
+        id: 5,
+        label: '二级 2-1'
+      }, {
+        id: 6,
+        label: '二级 2-2'
+      }]
+    }, {
+      id: 3,
+      label: '一级 3',
+      children: [{
+        id: 7,
+        label: '二级 3-1'
+      }, {
+        id: 8,
+        label: '二级 3-2'
+      }]
+    }]
     return {
-      pathInfo: ''
+      pathInfo: '',
+      data: JSON.parse(JSON.stringify(data))
     }
   },
   mounted() {
@@ -38,14 +99,36 @@ export default {
         //   this.isMaximize = false
         // }
       })
+    },
+    append(data) {
+      const newChild = { id: id++, label: 'testtest', children: [] }
+      if (!data.children) {
+        this.$set(data, 'children', [])
+      }
+      data.children.push(newChild)
+    },
+    remove(node, data) {
+      const parent = node.parent
+      const children = parent.data.children || parent.data
+      const index = children.findIndex(d => d.id === data.id)
+      children.splice(index, 1)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.path {
+.custom-tree-container {
   width: 100%;
   height: 100%;
+  overflow: auto;
+}
+.custom-tree-node {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
 }
 </style>
